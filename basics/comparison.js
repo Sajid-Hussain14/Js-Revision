3316a63af33063052a5b32f337ee65a519c3ce72
SAJID HUSSAIN GANIE <96110929+Sajid-Hussain14@users.noreply.github.com>
2024-11-11 07:51:52 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,15 @@
+/*********************COMPARIOSN OF DATATYPES***************************/
+
+//confused ones
+console.log("2">1) //returns true
+
+console.log(null == 0)
+console.log(null >= 0) //comparison convert null to a number treating it as zero that is why we will get true
+console.log(null > 0)   
+console.log(null < 0)  
+
+
+//===   type and value
+
+console.log("2" === 2) //false
+
