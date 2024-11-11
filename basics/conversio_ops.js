0bdf8219c0e7bc6c106e64a50ac7d90ad8131c0a
SAJID HUSSAIN GANIE <96110929+Sajid-Hussain14@users.noreply.github.com>
2024-11-11 07:20:49 +0000

    datatypes,conversion

---
+++ basics/conversio_ops.js
@@ -0,0 +1,32 @@
+let score1 = "23"
+let score2 = "23bcc"
+let score3 = null
+
+console.log(typeof score1)
+
+let val_number1 = Number(score1)
+let val_number2 = Number(score2)
+let val_number3 = Number(score3)
+
+
+
+/*console.log(typeof val_number1)
+console.log(val_number2) //NaN -->
+console.log(typeof val_number2)
+console.log(val_number3) //---0
+*/
+
+
+//when converting --> "23" is easy to convert in 23
+// "33bcc" => NaN
+//true ==1  and false ==0
+
+
+
+
+
+let isLogIn = "sajid"// TRUE coz` some value is there  //""---> FALSE //1---->TRUE
+
+let booleanIsLogIn = Boolean(isLogIn)
+
+console.log(booleanIsLogIn)
---
+++ basics/datatype.js
@@ -0,0 +1,28 @@
+// Its obvious that there is a difference between various of js
+// to use the newer version of js we use this......
+
+"use strict";
+/*For documentation you can choose any resoucre out there e.g: mdn,t39 -ecma etc.,
+*/
+
+// As this is a revision series so theoritical explanation will be less:
+
+// numubers => 2 to power 52 or 53
+
+//bigint => for big numbers
+
+// string => "" or ''
+// boolean => T/F
+// null => standalone value--empty value--- special type which can be used as well
+//  undefined => when a varibale is not initizaled only defined.
+// symbol => a unique type , useful when working with react etc.,
+
+//object =>
+
+let name ="sajid"
+let age = 23
+let num = null;
+
+console.log(typeof null) //SO null is an object
+
+
