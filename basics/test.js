f6d2b1f509e7334c13196c78a0183aa0fcb432d9
SAJID HUSSAIN GANIE <96110929+Sajid-Hussain14@users.noreply.github.com>
2024-11-11 05:50:03 +0000

    update and new file

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,18 @@
-console.log("sajid here")
+const acc_id = 1234    //declaring the constant
+let acc_email  = "sajid@yahoo.com"
+var acc_pw = "saj123"
+acc_add = "KMR" 
+let acc_state;
+// acc_id = 222
+
+/*
+ITS BETTER NOT TO USE var,
+DUE TO ITS SCOPE ISSUE
+*/
+
+acc_email = "hyus@ac.com"
+acc_pw = "hagsd123"
+acc_add ="jmu"
+
+// console.log(acc_id)
+console.table([acc_id, acc_email, acc_pw, acc_add, acc_state])
