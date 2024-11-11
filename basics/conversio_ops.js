fe4c89c9eebe051a3f2d28fdf5ed1a371733599c
SAJID HUSSAIN GANIE <96110929+Sajid-Hussain14@users.noreply.github.com>
2024-11-11 07:42:04 +0000

    conversion

--- basics/conversio_ops.js
+++ basics/conversio_ops.js
@@ -1,12 +1,12 @@
-let score1 = "23"
-let score2 = "23bcc"
-let score3 = null
+// let score1 = "23"
+// let score2 = "23bcc"
+// let score3 = null
 
-console.log(typeof score1)
+// console.log(typeof score1)
 
-let val_number1 = Number(score1)
-let val_number2 = Number(score2)
-let val_number3 = Number(score3)
+// let val_number1 = Number(score1)
+// let val_number2 = Number(score2)
+// let val_number3 = Number(score3)
 
 
 
@@ -25,8 +25,60 @@ console.log(val_number3) //---0
 
 
 
-let isLogIn = "sajid"// TRUE coz` some value is there  //""---> FALSE //1---->TRUE
+// let isLogIn = "sajid"// TRUE coz` some value is there  //""---> FALSE //1---->TRUE
 
-let booleanIsLogIn = Boolean(isLogIn)
+// let booleanIsLogIn = Boolean(isLogIn)
 
-console.log(booleanIsLogIn)
+// console.log(booleanIsLogIn)
+
+
+// *************************************OPerations*******************
+
+// let val = 3
+
+// let negVal = -val 
+
+// console.log(val)
+
+// console.log(negVal)
+
+
+let str1 = "hellooo"
+let str2 = "hyyyyyyy"
+
+let str3 = str1+str2
+
+// console.log(str3)
+// console.log("12"+3)  //>>>>output will be 123
+// console.log(12+"3")   /// same as above
+
+// console.log(1+2+"3")  //33--> 1+2=3+3 = 33 hahahahah nice js
+//move to mdn for other conversion and operations
+
+// console.log(+true)
+// console.log(+""
+// )
+
+// let n1,n2,n3;
+
+// n1 = n2 = n3 = 3+6
+
+
+
+// let gameCounter =100
+// // ++gameCounter;
+// gameCounter++;
+
+
+// console.log(gameCounter)
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
